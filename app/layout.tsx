10b07a41e2176e4bcc610b40ebe0994aaf217a7a
m220107 <220107ie@yse-c-net>
2024-01-10 12:16:29 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Navber from '@/app/components/Navbar'
+
 
 export const metadata: Metadata = {
   title: 'Next Tweet',
@@ -13,7 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        <Navber />
+        <main className='flex min-h-screen flex-col p-5'>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
