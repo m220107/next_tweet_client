537cfd5b3c6f249d0d95d5d427b8683ff6c00b7d
m220107 <220107ie@yse-c-net>
2024-01-24 12:14:40 +0900

    水曜日

--- app/auth/regist/page.tsx
+++ app/auth/regist/page.tsx
@@ -4,27 +4,31 @@ import { FaUser } from "react-icons/fa";
 import Input from "@/app/components/Input";
 import Link from "next/link";
 import { useState } from "react";
+import { registUser } from "@/app/services/UserService";
+import { useRouter } from "next/navigation";
+import ClickButton from "@/app/components/ClickButton";
 
 const RegistPage = () => {
-    const [name, setName] = useState<string>();
-    const [email, setEmail] = useState<string>();
-    const [password, setPassword] = useState<string>();
+    const router = useRouter();
+
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
     const regist = async () => {
-        const url = "http://localhost:8000/api/regist/store";
         console.log(name, email, password)
-
-        const response = await fetch(url,
-            {
-                method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ name, email, password }),
-            });
-        if (response.ok) {
-            const result = await response.json();
-            console.log(result);
+        // APIにデータ送信（ユーザ登録）
+        const result = await registUser({ name, email, password });
+        if (result.error) {
+            console.log(result.error)
+            // エラー表示
+        } else {
+            // トップページにリダイレクト
+            router.replace('/')
         }
     }
+
+    const isDisable = () => !(name && email && password);
 
     return (
         <div className="mx-auto w-1/3">
@@ -40,13 +44,11 @@ const RegistPage = () => {
             </div>
 
             <div>
-                <button className="
-                w-full bg-black
-                text-white hover:bg-gray-800
-                py-2 px-4 my-3
-                rounded-lg" onClick={regist}>
-                    Sign up
-                </button>
+                <ClickButton
+                    label="Sign up"
+                    onClick={regist}
+                    disabled={isDisable()}
+                />
 
                 <Link
                     href="/auth/login"
